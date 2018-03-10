86b0a3eb3ef9659c1accf7576d2b80c79414ff3e
Sai Haridass <sharidass1408@gmail.com>
2018-03-10 11:24:00 -0500

    Passing the path variable to service

--- src/main/java/com/haridass/springboot/basics/springbootin10steps/HelloWorldController.java
+++ src/main/java/com/haridass/springboot/basics/springbootin10steps/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.haridass.springboot.basics.springbootin10steps;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,15 @@ public class HelloWorldController {
 		return "Hello World";
 	}*/
 	
-	@GetMapping(path ="/hello-world-bean")
+	/*@GetMapping(path ="/hello-world-bean")
 	public HelloWorldBean helloBean(){
 		return new HelloWorldBean("Sai");		
+	}*/
+	
+	@GetMapping(path ="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+		return new HelloWorldBean(String.format("Hello World,%s", name));		
 	}
+	
 
 }
