5387fab443b1d7e9eb094ab331ece8c7f16798a9
Sai Haridass <sharidass1408@gmail.com>
2018-03-10 08:49:29 -0500

    HelloWorld Bean and Controller

--- src/main/java/com/haridass/springboot/basics/springbootin10steps/HelloWorldController.java
+++ src/main/java/com/haridass/springboot/basics/springbootin10steps/HelloWorldController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping(path = "/hello-world")
+	/*@GetMapping(path = "/hello-world")
 	public String helloWorld(){
 		return "Hello World";
+	}*/
+	
+	@GetMapping(path ="/hello-world-bean")
+	public HelloWorldBean helloBean(){
+		return new HelloWorldBean("Sai");		
 	}
 
 }
