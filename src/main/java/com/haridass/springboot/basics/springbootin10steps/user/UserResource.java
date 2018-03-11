77b57ef99ed5f408770caa07cdf97fb0f6acbd97
Sai Haridass <sharidass1408@gmail.com>
2018-03-11 10:25:27 -0400

    Code to get the URI location of the saved object

--- src/main/java/com/haridass/springboot/basics/springbootin10steps/user/UserResource.java
+++ src/main/java/com/haridass/springboot/basics/springbootin10steps/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.haridass.springboot.basics.springbootin10steps.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -28,8 +31,17 @@ public class UserResource {
 	}
 	
 	@PostMapping("/users")
-	public void saveUser(@RequestBody User user){
-		userDaoService.save(user);
+	public ResponseEntity<Object> saveUser(@RequestBody User user){
+		User savedUser = userDaoService.save(user);
+		// CREATED
+		// /user/{id} savedUser.getId()
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+		
 	}
 	
 }
