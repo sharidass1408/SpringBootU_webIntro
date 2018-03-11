b9ca8160ebf62e94d8fc02ec9819d55b80aea869
Sai Haridass <sharidass1408@gmail.com>
2018-03-11 13:19:32 -0400

    Deleting a user by Id

--- src/main/java/com/haridass/springboot/basics/springbootin10steps/user/UserResource.java
+++ src/main/java/com/haridass/springboot/basics/springbootin10steps/user/UserResource.java
@@ -1,11 +1,11 @@
 package com.haridass.springboot.basics.springbootin10steps.user;
 
 import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +49,19 @@ public class UserResource {
 		return ResponseEntity.created(location).build();
 		
 	}
+	
+	
+	@DeleteMapping("user/{id}")
+	public User deleteUser(@PathVariable int id){
+		User user = userDaoService.deleteById(id);
+		
+		if(user == null){
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		return user;
+	}
+	
+	
 	
 }
