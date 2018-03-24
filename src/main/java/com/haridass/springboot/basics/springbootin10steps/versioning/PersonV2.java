ab8e5283f832e80ff8984251ae370bb77da0d8e4
Sai Haridass <sharidass1408@gmail.com>
2018-03-24 13:16:57 -0400

    Versioning

---
+++ src/main/java/com/haridass/springboot/basics/springbootin10steps/versioning/PersonV2.java
@@ -0,0 +1,31 @@
+package com.haridass.springboot.basics.springbootin10steps.versioning;
+
+public class PersonV2 {
+	
+	private Name name;
+	
+	
+	
+	public PersonV2() {
+		super();
+	}
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	public void setName(Name name) {
+		this.name = name;
+	}
+
+	
+
+	
+	
+
+}
