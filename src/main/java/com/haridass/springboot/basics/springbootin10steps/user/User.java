edf337c431c8b865da7a38df45d08a0576d1440c
Sai Haridass <sharidass1408@gmail.com>
2018-03-25 20:09:12 -0400

    Adding Entity to the service, creating h2 database, storing data in H2

--- src/main/java/com/haridass/springboot/basics/springbootin10steps/user/User.java
+++ src/main/java/com/haridass/springboot/basics/springbootin10steps/user/User.java
@@ -2,11 +2,17 @@ package com.haridass.springboot.basics.springbootin10steps.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast two characters")
